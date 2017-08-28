7f76776eca10718d78d78c2251d3dbbebcd899e0
Marc S. Brooks <marc@mbrooks.info>
2017-08-28 00:56:15 -0700

    Added padding to timestamp output

--- bittrex_autotrader.py
+++ bittrex_autotrader.py
@@ -193,7 +193,7 @@ class BittrexAutoTrader(object):
         print humanfriendly.tables.format_pretty_table(
             stdout['rows'],
             stdout['cols']
-        ), "\n", time.strftime('%Y-%m-%d %H:%M:%S'), "\n"
+        ), "\n", time.strftime(' %Y-%m-%d %H:%M:%S '), "\n"
 
     @staticmethod
     def _list_of_dict_filter_by(data, key, value):
