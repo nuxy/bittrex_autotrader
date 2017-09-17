187cf72ba2e7b7994a9cdd3407e9e184b9f9e32a
Marc S. Brooks <marc@mbrooks.info>
2017-09-16 22:40:11 -0700

    Added PyPi package configuration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'bittrex_autotrader',
+  packages = ['bittrex_autotrader'],
+  version = '0.0.1',
+  description = 'Bittrex currency exchange autotrading script in a nutshell.',
+  author = 'Marc S. Brooks',
+  author_email = 'devel@mbrooks.info',
+  url = 'https://github.com/nuxy/bittrex_autotrader',
+  download_url = 'https://github.com/nuxy/bittrex_autotrader/archive/0.0.1.tar.gz',
+  keywords = ['trading-bot', 'api-client', 'cryptocurrency', 'bittrex'],
+  classifiers = [],
+)
