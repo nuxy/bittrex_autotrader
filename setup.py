d38b622d96e4a69cbc6af3b3f8ee3bcab2ba8eca
Marc S. Brooks <marc@mbrooks.info>
2017-09-29 02:19:58 -0700

    Updated release version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'bittrex_autotrader',
   packages = ['bittrex_autotrader'],
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'Bittrex currency exchange autotrading script in a nutshell.',
   author = 'Marc S. Brooks',
   author_email = 'devel@mbrooks.info',
   url = 'https://github.com/nuxy/bittrex_autotrader',
-  download_url = 'https://github.com/nuxy/bittrex_autotrader/archive/0.0.2.tar.gz',
+  download_url = 'https://github.com/nuxy/bittrex_autotrader/archive/0.0.3.tar.gz',
   keywords = ['trading-bot', 'api-client', 'cryptocurrency', 'bittrex'],
   classifiers = [],
 )
