60d3317c99ab48c0a23d037726bc3618a02792e7
Marc S. Brooks <marc@mbrooks.info>
2017-12-14 23:47:36 -0800

    Refactored so the version only needs to be defined once, per release
    Updated release version

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 
+version = '0.0.6'
+
 setup(
   name = 'bittrex_autotrader',
   packages = ['bittrex_autotrader'],
-  version = '0.0.5',
+  version = version,
   description = 'Bittrex currency exchange autotrading script in a nutshell.',
   author = 'Marc S. Brooks',
   author_email = 'devel@mbrooks.info',
   url = 'https://github.com/nuxy/bittrex_autotrader',
-  download_url = 'https://github.com/nuxy/bittrex_autotrader/archive/0.0.4.tar.gz',
+  download_url = 'https://github.com/nuxy/bittrex_autotrader/archive/0.0.{0}.tar.gz'.format(version),
   keywords = ['trading-bot', 'api-client', 'cryptocurrency', 'bittrex'],
   classifiers = [],
 )
